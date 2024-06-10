c8cb825114b19e987fad1aa0349f1f13fb6a11d9
nctan123 <canhtancanhtan@gmail.com>
2024-06-10 12:36:44 +0700

    get product price

--- BackEnd/JSSATSProject.Repository/Repos/DiamondPriceListRepository.cs
+++ BackEnd/JSSATSProject.Repository/Repos/DiamondPriceListRepository.cs
@@ -1,4 +1,5 @@
 ﻿using JSSATSProject.Repository.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace JSSATSProject.Repository.Repos;
 
@@ -6,5 +7,16 @@ public class DiamondPriceListRepository : GenericRepository<DiamondPriceList>
 {
     public DiamondPriceListRepository(DBContext context) : base(context)
     {
+    }
+
+    public async Task<decimal> FindPriceBy4CAndOrigin(int cutId, int clarityId, int colorId, int caratId, int originId)
+    {
+        var matchesDiamondPriceObject = await context.DiamondPriceLists.Where(d => d.CutId == cutId
+                                                               && d.CaratId == caratId
+                                                               && d.ClarityId == clarityId
+                                                               && d.ColorId == colorId
+                                                               && d.OriginId == originId)
+            .FirstOrDefaultAsync();
+        return matchesDiamondPriceObject!.Price;
     }
 }
