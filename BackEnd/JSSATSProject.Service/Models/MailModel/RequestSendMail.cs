f5992fa6a02593cb26c4c2231428dbc26818f996
NA <nhatanhtruong20@gmail.com>
2024-07-16 21:49:38 +0700

    fix RequestSendMail model

--- BackEnd/JSSATSProject.Service/Models/MailModel/RequestSendMail.cs
+++ BackEnd/JSSATSProject.Service/Models/MailModel/RequestSendMail.cs
@@ -2,5 +2,10 @@ namespace JSSATSProject.Service.Models.MailModel;
 
 public class RequestSendMail
 {
+    public required string ToAddress { get; set; }
+    public required int SellOrderId { get; set; }
+    public required decimal TotalPrice { get; set; }
+    public required decimal PromotionDiscount { get; set; }
+    public required decimal PointDiscount { get; set; }
     
 }
